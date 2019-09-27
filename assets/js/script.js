c39810b83a022b1a56cfbb529c0f907cb339e2cb
BritishPandaChick <sbartley1080@gmail.com>
2019-09-26 20:10:09 -0400

    Add code files

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var sh = 18;
+
+function dropSh(val){
+  var $txsh = $("p").css("text-shadow");
+  $("p").css("text-shadow", $txsh + "," + val + "px" + val + "px 0 #ff7373");
+}
+
+var i = 0;
+
+function myLoop(){
+  setTimeout(function(){
+    dropSh(i);
+    i++;
+
+    if(i < sh){
+      myLoop();
+    }
+  }, 5 * (i * 5/3))
+}
+
+myLoop();
