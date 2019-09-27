a443efe064c545ad57cd2e39e426c6ae23770665
BritishPandaChick <sbartley1080@gmail.com>
2019-09-27 19:01:11 -0400

    Update code files and links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 var sh = 18;
 
 function dropSh(val){
-  var $txsh = $('p').css('text-shadow');
-  $('p').css('text-shadow', $txsh + ',' + val + 'px' + val + 'px 0 #ff7373');
+	var $txsh = $('p').css('text-shadow');
+	$('p').css('text-shadow', $txsh + ', ' + val + 'px ' + val + 'px 0 #ff7373');
 }
 
 var i = 0;
 
 function myLoop(){
-  setTimeout(function(){
-    dropSh(i);
-    i++;
-    if(i<sh){
-      myLoop();
-    }
-  }, 5 * (i * 5/3))
+	setTimeout(function(){
+		dropSh(i);
+		i++;
+		if (i<sh){
+			myLoop();
+		}
+	}, 5 * (i * 5 / 3))
 }
 
 myLoop();
